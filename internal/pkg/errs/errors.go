316f146f8db977adc121b4fc2a719186bda9d257
Beretta350 <beretta.gabrielpp@gmail.com>
2024-05-16 18:16:50 -0300

    Robust update

--- internal/pkg/errs/errors.go
+++ internal/pkg/errs/errors.go
@@ -4,4 +4,5 @@ import "errors"
 
 var (
 	ErrUsernameOrPasswordMismatch error = errors.New("invalid username or password")
+	ErrMissingDataInRequest       error = errors.New("missing data in request")
 )
